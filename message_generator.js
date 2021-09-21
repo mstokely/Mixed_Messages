3064fd1055fb6fa48fc3c9b0c9172ce406fc2877
Mark Stokely <23437877+mstokely@users.noreply.github.com>
2021-09-21 14:46:28 -0400

    created message generator function.

--- message_generator.js
+++ message_generator.js
@@ -1,6 +1,28 @@
 const prompt = require('prompt-sync')();
 
+const { Console } = require('console');
 const messageBank = require('./message_bank');
 
 let userName = prompt("Hello, what is your name?");
-console.log(userName);
+let desireToHearJoke = prompt(`Hello ${userName}, would you like to hear a joke?`);
+
+let desiredJoke = prompt('Great! What type of joke would you like to hear?  Your options are puns, dadJokes or oneLiners.');
+let jokeType = '';
+let jokeGenerator = function(jokeInput){
+     if (jokeInput == 'puns'){
+        jokeType = 'pun';
+        return messageBank[0][Math.floor(Math.random() * 5)]
+     };
+     if(jokeInput =='dadJokes') {
+         jokeType = 'dad joke';
+         return messageBank[1][Math.floor(Math.random() * 5)]
+     };
+     if(jokeInput == 'oneLiners') {
+         jokeType = 'one liner';
+         return messageBank[2][Math.floor(Math.random() * 5)]
+     }
+};
+
+ let randomJoke = jokeGenerator(desiredJoke);
+
+console.log(`Awesome. So ${userName}, what do you think of this ${jokeType}:` + randomJoke);
