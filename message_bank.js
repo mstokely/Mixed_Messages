634acfaa026433ddf79dd438a1b52f17a8e0f7e3
Mark Stokely <23437877+mstokely@users.noreply.github.com>
2021-09-21 13:09:50 -0400

    added oneLiners array to message_bank.

--- message_bank.js
+++ message_bank.js
@@ -11,4 +11,10 @@ const dadJokes = ["I'm afraid for the calendar. Its days are numbered.",
 "I only know 25 letters of the alphabet. I don't know y.",
 "I asked my dog what's two minus two. He said nothing."];
 
-console.log(puns);
+const oneLiners = ["As I suspected, someone has been adding soil to my garden. The plot thickens.",
+"I have a joke about trickle down economics. But 99% of you will never get it.",
+"Did you hear about the perfume that smells of nothing? I think it's total non-scents.",
+"Bigfoot is sometimes confused with Sasquatch, Yeti never complains.",
+"The inventor of the throat lozenge died last month. There was no coffin at his funeral."];
+
+console.log(oneLiners);
