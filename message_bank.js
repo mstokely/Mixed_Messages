5cb905daba41479c2bbc363a505b639da9332305
Mark Stokely <23437877+mstokely@users.noreply.github.com>
2021-09-21 13:01:53 -0400

    updated dadJokes array.

--- message_bank.js
+++ message_bank.js
@@ -2,7 +2,7 @@
 const puns = [""];
 
 const dadJokes = ["I'm afraid for the calendar. Its days are numbered.",
-"What do you call a fish wearing a bowtie?" "Sofishticated.",
+"What do you call a fish wearing a bowtie? Sofishticated.",
 "Dear Math, grow up and solve your own problems.",
 "I only know 25 letters of the alphabet. I don't know y.",
 "I asked my dog what's two minus two. He said nothing."];
