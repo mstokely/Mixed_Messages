5c76c70d4ed05fe7c71e13e0538b075ca6e027ec
Mark Stokely <23437877+mstokely@users.noreply.github.com>
2021-09-21 13:06:07 -0400

    added puns array to message_bank.

--- message_bank.js
+++ message_bank.js
@@ -1,5 +1,9 @@
 
-const puns = [""];
+const puns = ["I'm reading a book about anti-gravity. It's impossible to put down!",
+"The past, the present, and the future walk into a bar. It was tense!", 
+"How did the picture end up in jail? It was framed!",
+"You really shouldn't be intimidated by advanced math…it's easy as pi!",
+"My ex-wife still misses me. But her aim is starting to improve!"];
 
 const dadJokes = ["I'm afraid for the calendar. Its days are numbered.",
 "What do you call a fish wearing a bowtie? Sofishticated.",
@@ -7,4 +11,4 @@ const dadJokes = ["I'm afraid for the calendar. Its days are numbered.",
 "I only know 25 letters of the alphabet. I don't know y.",
 "I asked my dog what's two minus two. He said nothing."];
 
-console.log(dadJokes);
+console.log(puns);
